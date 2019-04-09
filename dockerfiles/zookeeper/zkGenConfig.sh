ac47aec45d4ecceb70332b90d515d74bdb0a4ee7
solomonlinux <1211348968@qq.com>
2019-04-09 04:26:36 -0400

    test

--- dockerfiles/zookeeper/zkGenConfig.sh
+++ dockerfiles/zookeeper/zkGenConfig.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 ZK_USER=${ZK_USER:-"zookeeper"}
 ZK_LOG_LEVEL=${ZK_LOG_LEVEL:-"INFO"}
 ZK_DATA_DIR=${ZK_DATA_DIR:-"/var/lib/zookeeper/data"}
@@ -15,6 +17,7 @@ ZK_SYNC_LIMIT=${ZK_SYNC_LIMIT:-5}
 ZK_HEAP_SIZE=${ZK_HEAP_SIZE:-2G}
 ZK_MAX_CLIENT_CNXNS=${ZK_MAX_CLIENT_CNXNS:-60}
 ZK_MIN_SESSION_TIMEOUT=${ZK_MIN_SESSION_TIMEOUT:-$((ZK_TICK_TIME*2))}
+ZK_MAX_SESSION_TIMEOUT=${ZK_MAX_SESSION_TIMEOUT:-$((ZK_TICK_TIME*20))}
 ZK_SNAP_RETAIN_COUNT=${ZK_SNAP_RETAIN_COUNT:-3}
 ZK_PURGE_INTERVAL=${ZK_PURGE_INTERVAL:-0}
 ID_FILE="$ZK_DATA_DIR/myid"
@@ -26,7 +29,7 @@ DOMAIN=`hostname -d`
 
 function print_servers(){
 	for ((I=0; I<$ZK_REPLICAS; I++)); do
-		echo "server.${I}=${HOST}.${DOMAIN}:${ZK_SERVER_PORT}:${ZK_ELECTION_PORT}"
+		echo "server.$I=$HOST.$DOMAIN:$ZK_SERVER_PORT:$ZK_ELECTION_PORT"
 	done
 }
 
@@ -39,9 +42,10 @@ function validate_env(){
 	fi
 
 	if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
-		MY_ID=$(echo ${HOST} | cut -d- -f2)
+		MY_ID=$(echo $HOST | cut -d- -f2)
 	else
-		echo "Failed to extract ordinal from hostname ${HOST}"
+		echo "Failed to extract ordinal from hostname $HOST"
+		exit 1
 	fi
 
 	echo "ZK_REPLICAS=$ZK_REPLICAS"
