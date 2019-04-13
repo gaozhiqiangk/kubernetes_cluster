9efd30cc8c4139ddcb410c7317f151298925910b
solomonlinux <1211348968@qq.com>
2019-04-13 19:15:41 -0400

    test

--- Dockerfiles/kafka/entrypoint.sh
+++ Dockerfiles/kafka/entrypoint.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# broker.id=0
-BROKER_ID=echo $POD_NAME | cut -d- -f2
-sed -i "s/broker\.id=.*/broker\.id=${BROKER_ID}/g" /opt/kafka/config/server.properties
+set -e
 
-# log.dirs=/tmp/kafka-logs
-sed -i "s/log\.dirs=.*/log\.dirs={$LOG_DIRS}/g" /opt/kafka/config/server.properties
+HOST=`hostname -s`
+DOMAIN=`hostname -d`
+BROKER_ID=${HOST##*-}
 
-# num.partitions=1
-sed -i "s/num\.partitions=.*/num\.partitions=${NUM_PARTITIONS}/g" /opt/kafka/config/server.properties
 
-# zookeeper.connect=localhost:2181
-sed -i "s/zookeeper\.connect=.*/zookeeper\.connect=${ZOOKEEPER_CONNECT}/" /opt/kafka/config/server.properties
+CONF_DIR=/opt/kafka/config
 
-# group.initial.rebalance.delay.ms=0
-sed -i "s/group\.initial\.rebalance\.delay\.ms=.*/group\.initial\.reblance\.delay\.ms=${GROUP_INITIAL_REBALANCE_DELAY_MS}/" /opt/kafka/config/server.properties
+function create_server_properties() {
+	mv $CONF_DIR/server.properties{,.bak}
+	broker.id=$BROKER_ID
+	#listeners=PLAINTEST://:9002
+	#advertised.listeners=PLAINTEXT://your.host.name:9092
+	#listener.security.protocol.map=PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
+	num.network.threads=3
+	num.io.threads=3
+	socket.send.buffer.bytes=102400
+	socket.receive.buffer.bytes=102400
+	socket.request.max.bytes=104857600
+	log.dirs=/tmp/kafka-logs
+	num.partitions=1
+	num.recovery.threads.per.data.dir=1
+	offsets.topic.replication.factor=1
+	transaction.state.log.min.isr=1
+	#log.flush.interval.messages=10000
+	#log.flush.interval.ms=1000
+	log.retention.hours=168
+	#log.retention.bytes=1073741824
+	log.segment.bytes=1073741824
+	log.retention.check.interval.ms=300000
+	zookeeper.connect=localhost:2181
+	zookeeper.connection.timeout.ms=6000
+	group.interval.rebalance.delay.ms=0
+
+}
 
 exec "$@"
